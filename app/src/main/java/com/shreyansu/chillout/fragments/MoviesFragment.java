5dbf1893d9422734676c396544518e1bd460edd9
Shreyansu <sraj221099@gmail.com>
2020-12-08 12:21:35 +0530

    Working on Fragments

--- app/src/main/java/com/shreyansu/chillout/fragments/MoviesFragment.java
+++ app/src/main/java/com/shreyansu/chillout/fragments/MoviesFragment.java
@@ -3,60 +3,88 @@ package com.shreyansu.chillout.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.shreyansu.chillout.R;
 
 
-public class MoviesFragment extends Fragment {
+public class MoviesFragment extends Fragment
+{
+    private ProgressBar kprogressBar;
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public MoviesFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment MoviesFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static MoviesFragment newInstance(String param1, String param2) {
-//        MoviesFragment fragment = new MoviesFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+    private FrameLayout kNowshowinglayout;
+    private FrameLayout kupcominglayout;
+    private FrameLayout kpopularlayout;
+    private FrameLayout ktopratedlayout;
+
+
+    private TextView knowShowingAllText;
+    private TextView kpopularAllText;
+    private TextView kupcomingAllText;
+    private TextView ktopratedALLText;
+
+
+    private boolean knowshowingsectionload;
+    private boolean kpopularmovieload;
+    private boolean kupcomingmovieload;
+    private boolean ktopratedmovieload;
+
+    private RecyclerView knowshowingRecyView;
+    private RecyclerView kpopularRecycView;
+    private RecyclerView kupcomingRecycView;
+    private RecyclerView ktopRatedRecycView;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View view= inflater.inflate(R.layout.fragment_movies, container, false);
+
+
+        kprogressBar=(ProgressBar)view.findViewById(R.id.progress_bar);
+        kprogressBar.setVisibility(view.GONE);
+
+        knowshowingsectionload=false;
+        kpopularmovieload=false;
+        kupcomingmovieload=false;
+        ktopratedmovieload=false;
+
+        kNowshowinglayout=(FrameLayout)view.findViewById(R.id.now_showing);
+        kpopularlayout=(FrameLayout)view.findViewById(R.id.popular);
+        kupcominglayout=(FrameLayout)view.findViewById(R.id.upcoming);
+        ktopratedlayout=(FrameLayout)view.findViewById(R.id.top_rated);
+
+        knowShowingAllText=(TextView)view.findViewById(R.id.all_showing_now);
+        kupcomingAllText=(TextView)view.findViewById(R.id.text_view_all_upcoming);
+        kpopularAllText=(TextView)view.findViewById(R.id.text_view_all_popular);
+        ktopratedALLText=(TextView)view.findViewById(R.id.all_top_rated);
+
+
+        knowshowingRecyView=(RecyclerView)view.findViewById(R.id.recycler_view_now_showing);
+        (new LinearSnapHelper()).attachToRecyclerView(knowshowingRecyView);
+
+        kpopularRecycView=(RecyclerView)view.findViewById(R.id.recycler_view_popular);
+        kupcomingRecycView=(RecyclerView)view.findViewById(R.id.recycler_view_upcoming);
+        (new LinearSnapHelper()).attachToRecyclerView(kupcomingRecycView);
+
+        ktopRatedRecycView=(RecyclerView)view.findViewById(R.id.recycler_view_top_rated);
+        //TODO 9-12
+
+
+
+        return view;
     }
+
+
 }
